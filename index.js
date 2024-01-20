bd2c9307de7e1fea7e0ad306a8639ec611519c25
Siriratda <65030240@kmitl.ac.th>
2024-01-21 01:03:42 +0700

    คำอธิบายการเปลี่ยนแปลง

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 const port = process.env.PORT || 3002;
 
 app.get('/',(req,res) =>{
-    res.send("Hellooooooooooooo สสัสสสส")
+    res.send("Hellooooooooooooo ในที่สุดก็ได้แล้วสัสสสสเอ้ยยยยยยย")
+})
+
+app.get('/pp',(req,res) =>{
+    res.send("กู Deployed Nodejs ได้แล้วไอเวรรรรร")
 })
 
 app.listen(port,()=>{
